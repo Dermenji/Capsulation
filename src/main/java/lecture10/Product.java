38f39f493dc2282eb9f5dcc1d1164ca850313d5c
Alex Dermenji <alex.dermenji@gmail.com>
2021-02-15 01:00:14 +0200

    commit from Tortoise Git

--- src/main/java/lecture10/Product.java
+++ src/main/java/lecture10/Product.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public class Product {
   private String name;
-  private double cost;
+  public double cost;
 
   public Product(String name, double cost) {
     setName(name);
